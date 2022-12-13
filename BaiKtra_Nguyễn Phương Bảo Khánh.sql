1e496fc988638e5dcdc43e8adf810823a4319df3
KhanhKhoaHoc <91831931+KhanhKhoaHoc@users.noreply.github.com>
2022-12-13 14:59:58 +0700

    Add files via upload

--- BaiKtra_Nguyễn Phương Bảo Khánh.sql
+++ BaiKtra_Nguyễn Phương Bảo Khánh.sql
@@ -26,5 +26,54 @@ CREATE TABLE HANGXUAT
 select top 1 (slban* dongia) as 'tong tien ', mahd 
  from  hangxuat
  order by [tong tien ] 
-
- --Cau 3
+ 
+--Cau 3
+CREATE FUNCTION f3 (
+    @MAHD varchar(10)
+)
+RETURNS TABLE
+AS
+RETURN
+    SELECT 
+        HX.MAHD,
+        HD.NGAYXUAT,
+        HD.MAVT,
+        HX.DONGIA,
+        HX.SLBAN,  
+        CASE
+            WHEN WEEKDAY(HD.NGAYXUAT) = 0 THEN N'Th? hai'            
+            WHEN WEEKDAY(HD.NGAYXUAT) = 1 THEN N'Th? ba'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 2 THEN N'Th? t?'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 3 THEN N'Th? n?m'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 4 THEN N'Th? sáu'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 5 THEN N'Th? b?y'
+            ELSE N'Ch? nh?t'
+        END AS NGAYTHU
+    FROM HANGXUAT HX
+    INNER JOIN HDBAN HD ON HX.MAHD = HD.MAHD
+    WHERE HX.MAHD = @MAHD;
+--Cau 4
+CREATE FUNCTION f3 (
+    @MAHD varchar(10)
+)
+RETURNS TABLE
+AS
+RETURN
+    SELECT 
+        HX.MAHD,
+        HD.NGAYXUAT,
+        HD.MAVT,
+        HX.DONGIA,
+        HX.SLBAN,  
+        CASE
+            WHEN WEEKDAY(HD.NGAYXUAT) = 0 THEN N'Th? hai'            
+            WHEN WEEKDAY(HD.NGAYXUAT) = 1 THEN N'Th? ba'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 2 THEN N'Th? t?'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 3 THEN N'Th? n?m'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 4 THEN N'Th? sáu'
+            WHEN WEEKDAY(HD.NGAYXUAT) = 5 THEN N'Th? b?y'
+            ELSE N'Ch? nh?t'
+        END AS NGAYTHU
+    FROM HANGXUAT HX
+    INNER JOIN HDBAN HD ON HX.MAHD = HD.MAHD
+    WHERE HX.MAHD = @MAHD;
